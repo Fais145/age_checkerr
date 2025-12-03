10f3a6fa354760f36bbe769a00a393c1d6334c07
Mitchell <mitch.lewsey@gmail.com>
2025-12-03 15:12:40 +0000

    setup base files

---
+++ tests/test_age_checkerr.py
@@ -0,0 +1,36 @@
+# Age Checker Function Design Recipe
+
+## 1. Describe the Problem
+"""
+As an admin
+So that I can determine whether a user is old enough
+I want to allow them to enter their date of birth as a string in the format `YYYY-MM-DD`.
+
+As an admin
+So that under-age users can be denied entry
+I want to send a message to any user under the age of 16 saying their access is denied
+And telling them their current age and the required age (16).
+
+As an admin
+So that old enough users can be granted access
+I want to send a message to any user aged 16 or older to say that access has been granted.
+
+## 2. Design the Function Signature
+
+_Include the name of the function, its parameters, return value, and side effects._
+
+```python
+
+imports: datetime from datetime
+
+def age_checker(date)
+    Converts date into datetime object and checks if their age is <=16
+
+    Parameters: (list all parameters and their types)
+    date as string
+
+    Returns: (state the return value and its type)
+        returns string
+
+    Side effects: (state any side effects)
+"""
